67d1322b1cd8c555be694f403d58b9ac6a42a921
Phil Opaola <phillipsopaolacoding@gmail.com>
2018-03-26 03:16:50 -0400

    add basic command line interface

--- main.go
+++ main.go
@@ -1,19 +1,10 @@
 package main
 
-import (
-    "fmt"
-)
 
 func main() {
     bc := NewBlockChain()
+    defer bc.db.Close()
 
-    bc.AddBlock("Send 1 BTC to Phil")
-    bc.AddBlock("Send 2 BTC more to Phil")
-
-    for _, block := range bc.blocks {
-        fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-        fmt.Printf("Data: %s\n", block.Data)
-        fmt.Printf("Hash: %x\n", block.Hash)
-        fmt.Println()
-    }
+    cli := CLI{bc}
+    cli.Run()
 }
