14adf100f7c8e12f00e7d596d4caa9a06d75d623
Phil Opaola <phillipsopaolacoding@gmail.com>
2018-03-31 14:52:08 -0400

    implement transactions skeleton with vin and vout

--- block.go
+++ block.go
@@ -1,65 +1,68 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"encoding/gob"
-	"log"
-	"strconv"
-	"time"
+    "bytes"
+    "crypto/sha256"
+    "encoding/gob"
+    "log"
+    "time"
 )
 
 type Block struct {
-	Timestamp     int64
-	Data          []byte
-	PrevBlockHash []byte
-	Hash          []byte
-	Nonce         int
+    Timestamp     int64
+    Transactions  []*Transaction
+    PrevBlockHash []byte
+    Hash          []byte
+    Nonce         int
 }
 
-func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
-	pow := NewProofOfWork(block)
-	nonce, hash := pow.Run()
+func NewBlock(transactions []*Transaction, prevBlockHash []byte) *Block {
+    block := &Block{time.Now().Unix(), transactions, prevBlockHash, []byte{}, 0}
+    pow := NewProofOfWork(block)
+    nonce, hash := pow.Run()
 
-	block.Hash = hash[:]
-	block.Nonce = nonce
+    block.Hash = hash[:]
+    block.Nonce = nonce
 
-	return block
+    return block
 }
 
-func NewGenesisBlock() *Block {
-	return NewBlock("Genesis Block", []byte{})
+func NewGenesisBlock(coinbase *Transaction) *Block {
+    return NewBlock([]*Transaction{coinbase}, []byte{})
 }
 
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
+func (b *Block) HashTransactions() []byte {
+    var txHashes [][]byte
+    var txHash [32]byte
+    
+    for _, tx := range b.Transactions {
+        txHashes = append(txHashes, tx.ID)
+    }
+    txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
 
-	b.Hash = hash[:]
+    return txHash[:]
 }
 
 func (b *Block) Serialize() []byte {
-	var result bytes.Buffer
-	encoder := gob.NewEncoder(&result)
+    var result bytes.Buffer
+    encoder := gob.NewEncoder(&result)
 
-	err := encoder.Encode(b)
-	if err != nil {
-		log.Panic(err)
-	}
+    err := encoder.Encode(b)
+    if err != nil {
+        log.Panic(err)
+    }
 
-	return result.Bytes()
+    return result.Bytes()
 }
 
 func DeserializeBlock(d []byte) *Block {
-	var block Block
+    var block Block
 
-	decoder := gob.NewDecoder(bytes.NewReader(d))
-	err := decoder.Decode(&block)
-	if err != nil {
-		log.Panic(err)
-	}
+    decoder := gob.NewDecoder(bytes.NewReader(d))
+    err := decoder.Decode(&block)
+    if err != nil {
+        log.Panic(err)
+    }
 
-	return &block
+    return &block
 }
